32fbe6642d6970ca983a05d089d7f6b0b174af7d
je3f0o <je3f0o@gmail.com>
2015-09-30 21:39:51 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name  : gulp-requireng-concat.js
+* Purpose    :
+* Created at : 2015-09-30
+* Updated at : 2015-09-30
+* Author     : jeefo
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+"use strict";
+
+module.exports = require("./lib/event_dispatcher");
